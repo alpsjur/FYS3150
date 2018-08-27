79ed1a85d9ca7e7bd8b1b87b1a86bd1aa9a1dac9
janadr <cremesinus@gmail.com>
2018-08-27 22:15:11 +0200

    Kompilert program og produsert datafiler. Sjekker naa grafisk i python.

--- prosjekt1/cpp_kode/matrix_solver.cpp
+++ prosjekt1/cpp_kode/matrix_solver.cpp
@@ -1,49 +1,53 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 #include <cmath>
 
 using namespace std;
 
 
-void initialise_arrays(int n, double& f[], double h);
-void generate_gauss(int n,double& a[], double& b[], double& c[], double& d[]);
-void solve_vector(int n, double& v[],double& b[], double& c[], double& d[]);
-void write_data(int n, double v[]);
+void init(int n, double*& a, double*& b, double*& c, double*& d, double h);
+void generate_gauss(int n, double*& a, double*& b, double*& c, double*& d);
+void solve_vector(int n, double*& v, double*& b, double*& c, double*& d);
+void write_data(int n, double*& v);
 
 
-int main(int argc, int * argv[]) {
-  const int n = argv[1];
-  const int h = 1/(n + 1);
+int main(int argc, char * argv[]) {
+  int n = atof(argv[1]);
+  double h = 1.0/(n + 1);
 
-  double * new v[n];
-  double * new a[n];
-  double * new b[n];
-  double * new c[n];
-  double * new d[n];
+  double * v = new double [n];
+  double * a = new double [n];
+  double * b = new double [n];
+  double * c = new double [n];
+  double * d = new double [n];
 
-  initialise_arrays(n, b, h);
+  init(n, a, b, c, d, h);
   generate_gauss(n, a, b, c, d);
-  delete[n] a;
+  delete[] a;
   v[n] = d[n]/b[n];
   solve_vector(n, v, b, c, d);
-  delete[n] b, c, d;
+  delete[] b;
+  delete[] c;
+  delete[] d;
+  write_data(n, v);
+  delete[] v;
 
   return 0;
 }
 
 
-void initialise_arrays(int n, double& b[], double h) {
-  for(int i = 0; i < n+1; ++i) {
+void init(int n, double*& a, double*& b, double*& c, double*& d, double h) {
+  for(int i = 0; i < n; ++i) {
     a[i] = -1.0;
     b[i] = 2.0;
     c[i] = -1.0;
-    d[i] = h**2*100*exp(-10*i*h);
-
+    d[i] = pow(h, 2)*100.0*exp(-10.0*i*h);
   }
 }
 
 
-void generate_gauss(int n, double& a[], double& b[], double& c[], double& d[]) {
+void generate_gauss(int n, double*& a, double*& b, double*& c, double*& d) {
   for(int i = 2; i < n; ++i) {
     double k = a[i-1]/b[i-1];
     b[i] -= k*c[i-1];
@@ -52,16 +56,18 @@ void generate_gauss(int n, double& a[], double& b[], double& c[], double& d[]) {
 }
 
 
-void solve_vector(int n, double& v[],double& b[], double& c[], double& d[]) {
+void solve_vector(int n, double*& v, double*& b, double*& c, double*& d) {
   for(int i = n-1; i > 1; --i) {
     v[i] = (d[i-1] - b[i-1]*v[i-1])/c[i-1];
   }
 }
 
 
-void write_data(int n, double v[]) {
+void write_data(int n, double*& v) {
   ofstream datafile;
-  datafile.open("../data/matrix" << n << ".dat");
-  datafile << v;
+  datafile.open("../data/matrix" + to_string(n) + ".dat");
+  for(int i = 0; i < n; ++i) {
+    datafile << v[i] << " ";
+  }
   datafile.close();
 }
