25b27faac81f7e3fa7344ed7168f6fa918ec321a
janadr <cremesinus@gmail.com>
2018-08-27 23:39:26 +0200

    Fullfort programmet og lagd figur med python.

--- prosjekt1/cpp_kode/matrix_solver.cpp
+++ prosjekt1/cpp_kode/matrix_solver.cpp
@@ -9,12 +9,12 @@ using namespace std;
 void init(int n, double*& a, double*& b, double*& c, double*& d, double h);
 void generate_gauss(int n, double*& a, double*& b, double*& c, double*& d);
 void solve_vector(int n, double*& v, double*& b, double*& c, double*& d);
-void write_data(int n, double*& v);
+void write_data(int n, double* v);
 
 
 int main(int argc, char * argv[]) {
-  int n = atof(argv[1]);
-  double h = 1.0/(n + 1);
+  const int n = atof(argv[1]);
+  const double h = 1.0/(n + 1.0);
 
   double * v = new double [n];
   double * a = new double [n];
@@ -25,7 +25,7 @@ int main(int argc, char * argv[]) {
   init(n, a, b, c, d, h);
   generate_gauss(n, a, b, c, d);
   delete[] a;
-  v[n] = d[n]/b[n];
+  v[n] = (d[n]/b[n]);
   solve_vector(n, v, b, c, d);
   delete[] b;
   delete[] c;
@@ -38,7 +38,7 @@ int main(int argc, char * argv[]) {
 
 
 void init(int n, double*& a, double*& b, double*& c, double*& d, double h) {
-  for(int i = 0; i < n; ++i) {
+  for(int i = 0; i < n + 1; ++i) {
     a[i] = -1.0;
     b[i] = 2.0;
     c[i] = -1.0;
@@ -49,7 +49,7 @@ void init(int n, double*& a, double*& b, double*& c, double*& d, double h) {
 
 void generate_gauss(int n, double*& a, double*& b, double*& c, double*& d) {
   for(int i = 2; i < n; ++i) {
-    double k = a[i-1]/b[i-1];
+    const double k = a[i-1]/b[i-1];
     b[i] -= k*c[i-1];
     d[i] -= k*d[i-1];
   }
@@ -57,13 +57,13 @@ void generate_gauss(int n, double*& a, double*& b, double*& c, double*& d) {
 
 
 void solve_vector(int n, double*& v, double*& b, double*& c, double*& d) {
-  for(int i = n-1; i > 1; --i) {
-    v[i] = (d[i-1] - b[i-1]*v[i-1])/c[i-1];
+  for(int i = n-1; i > 0; --i) {
+    v[i] = (d[i] - c[i]*v[i+1])/b[i];
   }
 }
 
 
-void write_data(int n, double*& v) {
+void write_data(int n, double* v) {
   ofstream datafile;
   datafile.open("../data/matrix" + to_string(n) + ".dat");
   for(int i = 0; i < n; ++i) {
---
+++ prosjekt1/python_kode/plot_gauss.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+def u(x):
+    f = 1 - (1 - np.exp(-10))*x - np.exp(-10*x)
+    return f
+
+
+datalist = [10, 100, 1000]
+
+
+sns.set()
+sns.set_style("whitegrid")
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+legends = []
+for i in datalist:
+    data = np.loadtxt("../data/matrix{}.dat".format(i))
+    x = np.linspace(0, 1, len(data))
+    ax.plot(x, data)
+    legends.append('n = {}'.format(i))
+ax.plot(x, u(x))
+legends.append('analytic')
+
+ax.set_xlabel('x', fontsize=14)
+ax.set_ylabel('u(x)', fontsize=14)
+ax.legend(legends, fontsize=14)
+
+plt.savefig("../figurer/matrix_compare.pdf")
+plt.show()
