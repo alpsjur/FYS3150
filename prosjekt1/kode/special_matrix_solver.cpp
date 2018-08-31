b1d962e121aef036d5c8be59e0c6005c06a3599a
alpsjur <alpsjur@gmail.com>
2018-08-31 13:59:38 +0200

    Flyttet initsialiseringen av d ut fra algoritmen

--- prosjekt1/kode/special_matrix_solver.cpp
+++ prosjekt1/kode/special_matrix_solver.cpp
@@ -34,34 +34,33 @@ int main(int argc, char *arg[])
 
   try
   {
-    // initsialiserer d, b og x med dynamisk minnealkering
+    // initsialiserer d, b og x med dynamisk minnealokering
     double *b = new double[n];
+    double *d = new double[n];
     double *x = new double[n];
 
     b[0] = 2;
     x[0] = h;
+    d[0] =  hh*100*exp(-10.0*x[0]);
     for (int i = 1; i<n; ++i)
     {
       x[i] = (i+1)*h;
       b[i] = (double)(i+2)/(double)(i+1);
+      d[i] = hh*100.0*exp(-10.0*x[i]);
     }
 
     // initsialiserer v og d med dynamisk minnealkoasjon
-    double *d = new double[n];
     double *v = new double[n];
 
     clock_t c_start = clock();
     // forward substitution
-    d[0] =  hh*100*exp(-10.0*x[0]);
-
     for (int i = 1; i < n; ++i)
     {
-      d[i] = hh*100.0*exp(-10.0*x[i]) + d[i-1]/b[i-1];
+      d[i] =  d[i-1]/b[i-1];
     }
 
     // backward substitution
     v[n-1] = d[n-1]/b[n-1];
-
     for (int i = n-2; i >= 0; --i)
     {
       v[i] = (d[i] + v[i+1])/b[i];
@@ -69,6 +68,9 @@ int main(int argc, char *arg[])
     clock_t c_end = clock();
 
     cout << 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC << endl;
+
+    delete[] b;
+    delete[] d;
 
     /*
     ofstream datafile;
@@ -79,9 +81,6 @@ int main(int argc, char *arg[])
     }
     datafile.close()
     */
-
-    delete[] b;
-    delete[] d;
     delete[] x;
     delete[] v;
   }
