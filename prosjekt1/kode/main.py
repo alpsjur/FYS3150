6d35e6c6838424b07acbdab37ac886cfa8c53565
alpsjur <alpsjur@gmail.com>
2018-09-09 11:38:34 +0200

    kommenter pythonprogrammene

--- prosjekt1/kode/main.py
+++ prosjekt1/kode/main.py
@@ -1,3 +1,7 @@
+'''
+main python script for running all the c++ programs, plotting the error and
+printing the time ratio
+'''
 import os
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,21 +12,27 @@ def remove_file(filename):
     if os.path.exists(filename):
         os.system("rm " + filename)
 
+#first command line argument sets the number of runns (N) for each value of n
 N = int(sys.argv[1])
+#second command line argument sets the maximum power of n
 n_max = int(sys.argv[2])
+#making a list of n power values
 n = np.arange(1,n_max+1)
 
+#ask the user if the LU decomposition should the run.
 ans = input("Run LU decomposition? [y/n] ")
 if ans == 'y':
     LU = True
 else:
     LU = False
 
+#removing old data-logs
 remove_file("../data/general_matrix_time_log.dat")
 remove_file("../data/special_matrix_time_log.dat")
 remove_file("../data/LU_time_log.dat")
 remove_file("../data/max_error_log.dat")
 
+#running the c++ programs
 for i in n:
     for j in range(N):
         os.system("./general_matrix_solver.exe {}".format(i))
@@ -30,13 +40,14 @@ for i in n:
         if LU:
             os.system("./LU_decomp.exe {}".format(i))
 
-
+#load the generated data from the datalogs
 general_matrix_time = np.loadtxt("../data/general_matrix_time_log.dat")
 special_matrix_time = np.loadtxt("../data/special_matrix_time_log.dat")
 if LU:
     LU_time = np.loadtxt("../data/LU_time_log.dat")
 max_error = np.loadtxt("../data/max_error_log.dat")
 
+#plotting maximum error plot
 sns.set()
 sns.set_style("whitegrid")
 sns.set_palette("husl")
@@ -48,7 +59,8 @@ ax.set_ylabel(r'$\epsilon$', fontsize=14)
 plt.savefig("../figurer/error.pdf")
 plt.show()
 
-#Regner ut forholdet mellom CPU-tid for general og special
+#calculation the ratio between the CPU time of the special and general algorithm,
+#and the LU decomposition and special algorithm
 time_diff = np.zeros((n_max,3))
 for i in n:
     temp1 = general_matrix_time[(i-1)*N:i*N,1]/special_matrix_time[(i-1)*N:i*N,1]
@@ -58,4 +70,5 @@ for i in n:
         temp2 = np.nan
     time_diff[i-1] = np.array([i,np.mean(temp1), np.mean(temp2)])
 
+#printing the time ratio 
 print(time_diff)
