475dcff2cac146862173cd148b4f695b3214004b
alpsjur <alpsjur@gmail.com>
2018-09-13 23:04:18 +0200

    jobbet videre med funksjoner som trengs i jacobis metode

--- prosjekt2/Kode/jacobis_method.cpp
+++ prosjekt2/Kode/jacobis_method.cpp
@@ -8,11 +8,9 @@ using namespace std;
 using namespace arma;
 
 void initialize(mat &A, double *analytical_eigval,double a, double d, int n);
-
-//uttrykk for de analytiske egenverdiene
-inline double analytical(int i, int n, double a, double d){
-  return d+2*a*cos((i+1)*3.14159265/(n+1));
-}
+double analytical(int i, int n, double a, double d);
+void test_eigval(mat &A, double *analytical_eigval, int n);
+void find_largest(mat &A, int *k, int *l, int n);
 
 int main(int argc, char * argv[]) {
 
@@ -25,26 +23,61 @@ int main(int argc, char * argv[]) {
 
   //deklarerer matriser og vektorer
   mat A(n, n, fill::zeros);
+  mat R(n, n, fill::eye);     //matrix to hold eigenvectors as row elements
   double *analytical_eigval = new double [n];
 
   initialize(A, analytical_eigval, a, d, n);
 
-  //bruker armadillo for å regne ut egenverdiene
-  vec arma_eigval = eig_sym(A); //gir egenverdiene i økende størrelse
-  cout << arma_eigval << endl;
-  cout << analytical_eigval << endl;
+  test_eigval(A, analytical_eigval, n);
 
   return 0;
 }
 
 void initialize(mat &A, double *analytical_eigval,double a, double d, int n){
-  //setter diagonalelementene til a og elementene over og under til d
-  A(0,0) = a;
+  //setter diagonalelementene til a og elementene direkte over og under til d
+  A(0,0) = d;
   analytical_eigval[0] = analytical(0, n, a, d);
   for (int i=1; i < n; ++i) {
     analytical_eigval[i] = analytical(i, n, a, d);
-    A(i,i) = a;
-    A(i-1,i) = d;
-    A(i, i-1) = d;
+    A(i,i) = d;
+    A(i-1,i) = a;
+    A(i, i-1) = a;
+  }
+}
+
+double analytical(int i, int n, double a, double d){
+  const double pi = 3.14159;
+  return d+2.0*a*cos((i+1)*pi/(n+1.0));
+}
+
+void test_eigval(mat &A, double *analytical_eigval, int n){
+  const double tol = 1e-3;
+
+  //bruker armadillo for å regne ut egenverdiene
+  vec arma_eigval = eig_sym(A); //gir egenverdiene i økende rekkefølge
+
+  //itterer over egenverdiene og ser som differansen er innenfor tolleransen
+  //arma_eigval er i stigende rekkefølge, analytical_eigval er i synkende
+  for (int i=0; i<n;++i){
+    if (fabs(arma_eigval(i)-analytical_eigval[n-1-i])>tol){
+      cout << "Analytical and computed eigenvalues does not match" << endl;
+      cout << "Diference is " << fabs(arma_eigval(i)-analytical_eigval[n-1-i]) << endl;
+      break;
+    }
+  }
+}
+
+void find_largest(mat &A, int *k, int *l, int n) {
+  double largest = 0;
+  //ittererer over øvre halvdel av matrisen, siden matrisen er symmetrisk
+  for (int i=0; i<n; ++i){
+    for (int j=i+1;j<n;++j){
+      double aij = fabs(A(i,j));
+      if (aij>largest){
+        largest = aij;
+        *k = i;  //ER DETTE RIKTIG?!?!?!
+        *l = j;
+      }
+    }
   }
 }
