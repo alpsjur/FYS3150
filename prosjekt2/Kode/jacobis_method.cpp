92eb3e1ee151169ce44c60e05523581e9a8fcbe8
alpsjur <alpsjur@gmail.com>
2018-09-14 13:30:12 +0200

    jobbet mer på 1b, får feil egenverdier

--- prosjekt2/Kode/jacobis_method.cpp
+++ prosjekt2/Kode/jacobis_method.cpp
@@ -9,8 +9,9 @@ using namespace arma;
 
 void initialize(mat &A, double *analytical_eigval,double a, double d, int n);
 double analytical(int i, int n, double a, double d);
-void test_eigval(mat &A, double *analytical_eigval, int n);
-void find_largest(mat &A, int *k, int *l, int n);
+void test_eigval(mat A, double *analytical_eigval, int n);
+double find_largest(mat A, int *k, int *l, int n);
+void transform(mat &A, mat &S,int k, int l, int n);
 
 int main(int argc, char * argv[]) {
 
@@ -23,13 +24,24 @@ int main(int argc, char * argv[]) {
 
   //deklarerer matriser og vektorer
   mat A(n, n, fill::zeros);
-  mat R(n, n, fill::eye);     //matrix to hold eigenvectors as row elements
+  mat S(n, n, fill::eye);     //matrix to hold eigenvectors as row elements
   double *analytical_eigval = new double [n];
 
   initialize(A, analytical_eigval, a, d, n);
 
   test_eigval(A, analytical_eigval, n);
 
+  double max = 10.0;
+  double tol = 1e-10;
+  int k, l;
+  while (max > tol){
+    max = find_largest(A, &k, &l, n);
+    transform(A, S, k, l, n);
+  }
+  cout << A << endl;
+  for (int i=0; i<n; ++i){
+    cout << analytical_eigval[i] << endl;
+  }
   return 0;
 }
 
@@ -43,6 +55,7 @@ void initialize(mat &A, double *analytical_eigval,double a, double d, int n){
     A(i-1,i) = a;
     A(i, i-1) = a;
   }
+  return;
 }
 
 double analytical(int i, int n, double a, double d){
@@ -50,7 +63,7 @@ double analytical(int i, int n, double a, double d){
   return d+2.0*a*cos((i+1)*pi/(n+1.0));
 }
 
-void test_eigval(mat &A, double *analytical_eigval, int n){
+void test_eigval(mat A, double *analytical_eigval, int n){
   const double tol = 1e-3;
 
   //bruker armadillo for å regne ut egenverdiene
@@ -65,10 +78,11 @@ void test_eigval(mat &A, double *analytical_eigval, int n){
       break;
     }
   }
+  return;
 }
 
-void find_largest(mat &A, int *k, int *l, int n) {
-  double largest = 0;
+double find_largest(mat A, int *k, int *l, int n) {
+  double largest = 0.0;
   //ittererer over øvre halvdel av matrisen, siden matrisen er symmetrisk
   for (int i=0; i<n; ++i){
     for (int j=i+1;j<n;++j){
@@ -80,4 +94,52 @@ void find_largest(mat &A, int *k, int *l, int n) {
       }
     }
   }
+  return largest;
+}
+
+void transform(mat &A, mat &S,int k, int l, int n) {
+  double tau, t;
+  tau = (A(l,l) - A(k,k))/(2.0*A(k,l));
+  //for å unngå loss of precission når t -> inf skriver vi om uttrykket
+  if (tau >= 0){
+    t = 1.0/(tau+sqrt(1.0+tau*tau));   //tangens theta
+  }
+  else {
+    t = -1.0/(-tau+sqrt(1.0+tau*tau)); //tangens theta
+  }
+
+  double c, cc, s, ss, cs;
+  c = 1.0/(sqrt(1+t*t));             //cosinus theta
+  cc = c*c;
+  s = c*t;                           //sinus theta
+  ss = s*s;
+  cs = c*s;
+
+  //oppdaterer A
+  double akk, all, akl;
+  akk = A(k,k);
+  all = A(l,l);
+  akl = A(k,l);
+  A(k,k) = akk*cc - 2*akl*cs + all*ss;
+  A(l,l) = all*cc + 2*akl*cs + akk*ss;
+  A(k,l) = 0.0;//(akk-all)*cs + akl*(cc-ss);
+  A(l,k) = 0.0;//A(k,l);
+  for (int i=0; i<n; ++i) {
+    if (i != k && i != k) {
+      double aik, ail;
+      aik = A(i,k);
+      ail = A(i,l);
+      A(i,k) = aik*c - ail*s;
+      A(k,i) = A(i,k);
+      A(i,l) = ail*c + aik*s;
+      A(l,i) = A(i,l);
+    }
+    //kalkulerer egenvektorene
+    double sik, sil;
+    sik = S(i,k);
+    sil = S(i,l);
+    S(i,k) = c*sik - s*sil;
+    S(i,l) = c*sil + s*sik;
+  }
+  return;
 }
