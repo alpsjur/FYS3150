af42641f257516f8c4c5635914119453f674d874
alpsjur <alpsjur@gmail.com>
2018-10-05 14:02:39 +0200

    skrevet om ODE så de tar Coordinate

--- prosjekt3/kode/diffeq_solvers.cpp
+++ prosjekt3/kode/diffeq_solvers.cpp
@@ -13,3 +13,20 @@ void velocity_verlet(double dt, double *acc, double *pos, double *vel){
     acc[dim] = acc_new;
   }
 }
+
+void velocityVerlet(double dt, Coordinate &acc, Coordinate &pos, Coordinate &vel){
+  double dt = dt/2.0;
+  double 2dt2 = dt*dt/2.0;
+  Coordinate acc_new;
+
+  pos = pos + dt*vel + 2dt2*acc;
+  acc_new = calculate_acc(pos);
+  vel = vel + dt2*(acc_new + acc);
+  acc = acc_new; 
+}
+
+void forwardEuler(double dt, Coordinate &pos, Coordinate &vel){
+  Coordinate acc = calculate_acc(pos);
+  pos = pos + vel*dt;
+  vel = vel + acc*dt;
+}
--- prosjekt3/kode/system.cpp
+++ prosjekt3/kode/system.cpp
@@ -28,8 +28,8 @@ private:
   double m_mass;
   Coordinate m_initPos;
   Coordinate m_initVel;
-  Coordinate *m_pos;
-  Coordinate *m_vel;
+  //Coordinate *m_pos;
+  //Coordinate *m_vel;
 
 public:
   // A specific constructor
@@ -38,6 +38,7 @@ public:
   }
   Body() : m_name("Navnløs"), m_mass(0), m_initPos(Coordinate()), m_initVel(Coordinate()){
   }
+  /*
   void initArray(int arrayLength){
     m_pos = new Coordinate[arrayLength];
     m_vel = new Coordinate[arrayLength];
@@ -51,35 +52,31 @@ public:
   ~Body(){
     delete[] m_pos, m_vel;
   }
+  */
 };
 
 class System {
 private:
-  Body *m_bodies;
+  Body m_bodies[];
   int m_numberofBodies;
   int m_arrayLength;
   double m_endtime;
   double m_dt;
 
 public:
-  System(Body *bodies, int numberofBodies, double endtime, double dt)
+  System(Body bodies[], int numberofBodies, double endtime, double dt)
     :m_numberofBodies(numberofBodies), m_endtime(endtime), m_dt(dt){
-    m_bodies = new Body[m_numberofBodies];
     m_arrayLength = (int) m_endtime/m_dt;
     for (int i=0;i<m_numberofBodies;i++){
       m_bodies[i]=bodies[i];
-      m_bodies[i].initArray(m_arrayLength);
     }
-  }
-
-  ~System(){
-    delete[] m_bodies;
   }
 };
 
 int main(){
   Body jorda("Jorda", 300, Coordinate(1,1,1), Coordinate(2,2,2));
   Body jupiter("Jupiter", 600, Coordinate(2,2,2), Coordinate(1.5,1.5,1.5));
-  Body system1[2] = {jorda, jupiter};
+  Body bodies1[2] = {jorda, jupiter};
+  System jordajupiter(bodies1, 2, 10, 0.001);
   return 0;
 }
